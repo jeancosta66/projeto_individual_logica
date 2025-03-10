a483a4663875c0df2e07551dd198f0167a9fb6dc
jeancosta66 <costajean1005@gmail.com>
2025-03-10 18:52:41 -0300

    Create crescente.py

---
+++ crescente.py
@@ -0,0 +1,7 @@
+# def retorna o resultado 
+def ordenar_lista():
+    lista = [8, 2, 1, 99, 22, 11]
+    lista.sort() # Ordena a lista em ordem crescente
+    print( "Lista ordenada", lista)
+    
+ordenar_lista()    
