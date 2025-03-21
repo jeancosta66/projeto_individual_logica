0b6f38e3cc9278d609994029351700cbfa65ff74
jeancosta66 <costajean1005@gmail.com>
2025-03-21 07:18:51 -0300

    Create listar_descrecente.py

---
+++ listar_descrecente.py
@@ -0,0 +1,6 @@
+def ordenar_lista_descrecente():
+    lista = [1, 2, 9, 10, 22, 4]
+    lista.sort(reverse=True) # reverse=True, serve para ordenar na ordem descrecente 
+    print("Lista descrecente", lista)
+    
+ordenar_lista_descrecente()
